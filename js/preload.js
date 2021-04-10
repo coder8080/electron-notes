ae244b87206c47e342d6ce8ab0e12e60b40111a6
Роман Колеров <coder8080@mail.ru>
2021-04-10 20:54:14 +0300

    added ability to change notes

--- js/preload.js
+++ js/preload.js
@@ -22,7 +22,7 @@ window.addEventListener('init-for-profile-page', () => {
 })
 
 window.addEventListener('init-for-note-page', () => {
-    let id = document.getElementById('id-input').value
+    const id = document.getElementById('id-input').value
     let remove_button = document.getElementById('remove-button')
     let change_button = document.getElementById('change-button')
     let go_on_main_button = document.getElementById('go-on-main-button')
@@ -32,7 +32,7 @@ window.addEventListener('init-for-note-page', () => {
     })
     change_button.addEventListener('click', (e) => {
         e.preventDefault()
-        ipc.send('change-note', id)
+        ipc.send('go-on-change-note-page', id)
     })
     go_on_main_button.addEventListener('click', (e) => {
         e.preventDefault()
@@ -41,12 +41,11 @@ window.addEventListener('init-for-note-page', () => {
 })
 
 window.addEventListener('init-for-new-note-page', () => {
-    console.log('new note')
-    let form = document.getElementById('create-note-form')
+    const form = document.getElementById('create-note-form')
     console.log(form)
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', function(e) {
         e.preventDefault()
-        let data = new FormData(form)
+        const data = new FormData(this)
         const heading = data.get('heading')
         const text = data.get('text')
         ipc.send('create-note', heading, text)
@@ -55,5 +54,22 @@ window.addEventListener('init-for-new-note-page', () => {
     cancel_button.addEventListener('click', (e) => {
         e.preventDefault()
         ipc.send('go-on-main')
+    })
+})
+
+window.addEventListener('init-for-change-note-page', () => {
+    const id = document.getElementById('id-input').value
+    const form = document.getElementById('change-note-form')
+    const cancel_button = document.getElementById('cancel-button')
+    form.addEventListener('submit', function(e) {
+        e.preventDefault()
+        const data = new FormData(this)
+        const heading = data.get('heading')
+        const text = data.get('text')
+        ipc.send('change-note', id, heading, text)
+    })
+    cancel_button.addEventListener('click', (e) => {
+        e.preventDefault()
+        ipc.send('view-note', id)
     })
 })
--- main.js
+++ main.js
@@ -22,9 +22,10 @@ function createWindow() {
             preload: path.join(__dirname, 'js', 'preload.js')
         }
     })
-    go_on_main(win)
+    go_on_main()
 }
 
+/* Функция переадресации на главную страницу */
 function go_on_main() {
     db.all(`select * from notes`, (err, data) => {
         // Если произошла ошибка, то выкидываем её
@@ -60,14 +61,11 @@ function go_on_main() {
     })
 }
 
-app.whenReady().then(() => {
-    createWindow()
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
-
-ipcMain.on('view-note', (e, id) => {
+/**
+ * Переадресация на страницу записи
+ * @param {number} id - номер записи, на страницу которой нужно перейти
+ * */
+function go_on_note_page(id) {
     db.get(`select * from notes where id = ${id};`, (err, data) => {
         if (err) {
             console.log('app crashed when getting information from database with error:')
@@ -82,8 +80,20 @@ ipcMain.on('view-note', (e, id) => {
                 }
             })
         } else {
+            console.log('error: no note with this id')
         }
     })
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+ipcMain.on('view-note', (e, id) => {
+    go_on_note_page(id)
 })
 
 ipcMain.on('remove-note', (e, id) => {
@@ -115,8 +125,34 @@ ipcMain.on('create-note', (e, heading, text) => {
             console.log('app crashed when creating note with error:')
             throw err
         }
+        go_on_main()
     })
-    go_on_main()
+})
+
+ipcMain.on('go-on-change-note-page', (e, id) => {
+    db.get(`select * from notes where id = ${id};`, (err, data) => {
+        if (err) {
+            console.log('app crashed when getting info about note from db with error:')
+            throw err
+        }
+        ejs.data('note', data)
+        win.loadFile(path.join(__dirname, 'pages', 'change-note.ejs')).then(err => {
+            if (err) {
+                console.log('app crashed when showing new note page with error:')
+                throw err
+            }
+        })
+    })
+})
+
+ipcMain.on('change-note', (e, id, heading, text) => {
+    db.run(`update notes set heading = '${heading}', text = '${text}' where id = ${id};`, (err) => {
+        if (err) {
+            console.log('app crashed when updating info in database with error:')
+            throw err
+        }
+        go_on_note_page(id)
+    })
 })
 
 app.on('window-all-closed', () => {
