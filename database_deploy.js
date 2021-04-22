abad427aa2d2443b7c6bfa4085c5d5f7e7e5d051
Роман Колеров <coder8080@mail.ru>
2021-04-22 17:50:13 +0300

    database deploy script fix

--- database_deploy.js
+++ database_deploy.js
@@ -7,10 +7,22 @@ const path = require('path')
 fs.writeFileSync(path.join(__dirname, 'db.sqlite3'), '')
 
 const db = new sqlite.Database(path.join(__dirname, 'db.sqlite3'))
-db.run('create table notes (id integer primary key, heading text, text text); create table theme (id integer primary key, value varchar(10));', (err) => {
+db.run('create table notes (id integer primary key, heading text, text text);', (err) => {
     if (err) {
         console.log('Не удалось создать базу данных. Ошибка:')
         throw err
     }
-    console.log('База данных успешно создана и настроена.')
+    db.run('create table theme (id integer primary key, value varchar(10));', err => {
+        if (err) {
+            console.log('Не удалось создать базу данных. Ошибка:')
+            throw err
+        }
+        db.run(`insert into theme (value) values ('light');`, err => {
+            if (err) {
+                console.log('Не удалось настроить тему. Ошибка:')
+                throw err
+            }
+            console.log('База данных успешно создана и настроена.')
+        })
+    })
 })
